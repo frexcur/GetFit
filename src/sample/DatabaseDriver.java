a4c5c2874836ee59519a37575ef8e7bc60fe8000
Freiddy <fcuriel973@gmail.com>
2018-11-13 16:30:40 -0500

    More testing

--- src/sample/DatabaseDriver.java
+++ src/sample/DatabaseDriver.java
@@ -5,7 +5,25 @@ public class DatabaseDriver {
   public static void main(String[] args) {
     DBTest db = new DBTest();
     db.createConnection();
-    db.displayAge("frexcur");
+    int age = db.displayAge("frexcur");
+    System.out.println(age);
+    double weight = db.displayWeight("frexcur");
+    System.out.println(weight);
+
+    if (db.checkIfUsernameExist("fred")) {
+      System.out.println("account exist");
+    }
+    else {
+      System.out.println("wrong username or password");
+    }
+
+    if (db.checkIfPasswordIsCorrect("frexcur", "passher")){
+      System.out.println("correct password");
+    }
+    else {
+      System.out.println("wrong username or password");
+    }
+
     db.editAge("frexcur", 30);
     db.editWeight("frexcur", 400);
     db.editGender("frexcur", 'M');
